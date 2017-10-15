7e6b4a0c1618558cb355931ebdc4deddc6e4ae75
David McClure <davidwilliammcclure@gmail.com>
2017-10-15 16:12:03 -0400

    SQLAlchemy rig.

--- twitter_years/models.py
+++ twitter_years/models.py
@@ -2,21 +2,39 @@
 
 import glob
 import ujson
+import os
+import click
 
-from peewee import SqliteDatabase, Model, CharField, IntegerField
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy import create_engine, event, Column, Integer, String
+from sqlalchemy.engine.url import URL
+from sqlalchemy.ext.declarative import declarative_base
 
 
-db = SqliteDatabase('years.db')
+db_path = os.path.join(os.path.dirname(__file__), 'years.db')
+url = URL(drivername='sqlite', database=db_path)
+engine = create_engine(url)
+factory = sessionmaker(bind=engine)
+session = scoped_session(factory)
 
 
-class Year(Model):
+Base = declarative_base()
+Base.query = session.query_property()
 
-    class Meta:
-        database = db
 
-    prefix = CharField()
-    year = IntegerField()
-    suffix = CharField()
+class Year(Base):
+
+    __tablename__ = 'year'
+
+    __table_args__ = dict(sqlite_autoincrement=True)
+
+    id = Column(Integer, primary_key=True)
+
+    prefix = Column(String, nullable=False)
+
+    year = Column(Integer, nullable=False)
+
+    suffix = Column(String, nullable=False)
 
     @classmethod
     def load(cls, pattern):
@@ -26,8 +44,28 @@ class Year(Model):
             with open(path) as fh:
 
                 segment = [ujson.loads(line) for line in fh]
+                session.bulk_insert_mappings(cls, segment)
 
-                with db.atomic():
-                    cls.insert_many(segment).execute()
-
+                session.commit()
                 print(path)
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+def create():
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
+
+
+@cli.command()
+@click.argument('path', type=click.Path())
+def load(path):
+    Year.load(path)
+
+
+if __name__ == '__main__':
+    cli()
